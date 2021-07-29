fd66a499efa86bccc77826735d00c99583760a2e
maulikk2000 <maulikk2000@gmail.com>
2021-07-30 02:53:39 +1000

    topbar WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Topbar from './components/topbar/Topbar';
 
 function App() {
   return (
     <div className="App">
-     Hello
+     <Topbar />
     </div>
   );
 }
